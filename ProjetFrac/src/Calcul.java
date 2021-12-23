76a616f918dab30c404328f55cd5258947b39bbb
Alass <alassane.paris@gmail.com>
2021-12-23 06:25:39 +0100

    ajouté un builder pour calcul. non testé

--- ProjetFrac/src/Calcul.java
+++ ProjetFrac/src/Calcul.java
@@ -5,11 +5,109 @@ import java.lang.Math;
 
 public class Calcul {
 	public Complex comp;
-	public Function fun;
-	public int[][] colors = new int[201][201];
-	private final int matIter = 1000;
-	private final double radius = 2.0;
-	public Complex[][] graph = new Complex[201][201];
+	public int[] polynome;
+	public int[][] colors;
+	private final int maxIter;
+	private final double radius;
+	public Complex[][] graph;
+	public Complex topLeftComplex;
+	public double step;
+
+	public static class Builder {
+		//valeurs obligatoires : tabsizes et step ou tabsizes et plansize ou plansize et step
+		public Complex comp;
+		public int[] polynome = {0,0,1};
+		private int maxIter;
+		private double radius;
+		public Complex topLeftComplex;
+		public double step;
+		public int[] tabsizes = new int[2];
+		public double plansize;//TODO : mettre 2 coords
+		
+		public Builder() {
+			this.maxIter = 1000;
+			this.comp = new Complex(-0.7269, 0.1889);
+			this.step = 0.0;
+			this.plansize=0.0;
+			this.radius = 2.0;
+		}
+		public Builder size(int x,int y) {
+			this.tabsizes[0]= x;
+			this.tabsizes[1]= y;
+			return this;
+		}
+		public Builder step(double x) {
+			this.step=x;
+			return this;
+		}
+		public Builder maxIter(int x) {
+			this.maxIter=x;
+			return this;
+		}
+		public Builder topLeftComplex(Complex x) {
+			this.topLeftComplex=x;
+			return this;
+		}
+		public Builder comp(Complex x) {
+			this.comp=x;
+			return this;
+		}
+		public Builder polynome(int[] x) {
+			this.polynome=x;
+			return this;
+		}
+		public Builder radius(double x) {
+			this.radius=x;
+			return this;
+		}
+		public Calcul build() {
+			if (tabsizes==null) {
+				tabsizes[0]= (int) (plansize/step);
+				tabsizes[1] = (int) (plansize/step);//TODO: utiliser l'autre val de plansize	
+			}
+			if (plansize==0.0) {
+				plansize = step*tabsizes[0];
+			}
+			if (step==0.0){
+				step=plansize/tabsizes[0];
+			}
+			if(topLeftComplex==null)
+				this.topLeftComplex = new Complex(-plansize, plansize);
+			
+			return new Calcul(this);
+		}
+	}
+	public Calcul(Builder b) {
+		comp=b.comp;
+		polynome=b.polynome;
+		maxIter=b.maxIter;
+		radius=b.radius;
+		topLeftComplex=b.topLeftComplex;
+		step=b.step;
+		graph = new Complex[b.tabsizes[0]][b.tabsizes[1]];
+		colors= new int[b.tabsizes[0]][b.tabsizes[1]];
+	}
+
+	/*public Calcul(int x, int y, double h, Complex c) {
+		this.colors = new int[x][y];
+		this.graph = new Complex[x][y];
+		this.step = h;
+		this.topLeftComplex = c;
+		this.radius = 2.0;
+	}
+*/
+	public void setSize(int x, int y) {
+		colors = new int[x][y];
+		graph = new Complex[x][y];
+	}
+
+	public void setStep(int h) {
+		step = h;
+	}
+
+	public void setTopLeftComplex(Complex c) {
+		topLeftComplex = c;
+	}
 
 	double module(Complex z) {
 		return Math.sqrt(z.getReal() * z.getReal() + z.getImaginary() * z.getImaginary());
@@ -19,25 +117,26 @@ public class Calcul {
 		int ite = 0;
 		Complex zn = z0;
 		// sortie de boucle si divergence
-		while (ite < matIter && (Math.abs(module(zn))) <= radius) {
-			zn = zn.multiply(zn);		
-			zn=zn.add(comp);
+		while (ite < maxIter && (Math.abs(module(zn))) <= radius) {
+			while (ite < maxIter && (module(zn)) <= radius) {
+				zn = zn.multiply(zn);
+				zn = zn.add(comp);
 
-			
-			ite++;
+				ite++;
+			}
+
 		}
 		System.out.println(ite);
 
 		return ite;
+
 	}
 
 	Complex[][] fill() {
 		for (int i = 0; i < colors.length; i++) {
 			for (int j = 0; j < colors[0].length; j++) {
-				graph[i][j] = new Complex(-1 + j * 0.01, 1 - i * 0.01);
-			}
+				graph[i][j] = new Complex(topLeftComplex.getReal() + j * step, topLeftComplex.getImaginary() - i * step);			}
 		}
-		System.out.println(graph[100][100].getReal()+" "+graph[100][100].getImaginary());
 		return graph;
 	}
 
@@ -49,5 +148,4 @@ public class Calcul {
 		}
 		return colors;
 	}
-
 }
--- ProjetFrac/src/Main.java
+++ ProjetFrac/src/Main.java
@@ -3,24 +3,110 @@ import java.util.Scanner;
 import org.apache.commons.math3.complex.Complex;
 
 public class Main {
-	public int[] selectsize() {
+	public static void maker() {
+		Complex x = new Complex(-0.7269, 0.1889);
+		ImageGenerator g = new ImageGenerator();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("select height");
+		System.out.println("select one :" + '\n' +
+				"1 : step and imagesize" + '\n' + 
+				"2 : complex plan size and step"+ '\n' + 
+				"3 : imagesize and plan size");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		}
+	}
+
+	
+	
+	
+	public static int[] selectsize() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("choisir hauteur");
 		// String input
 		int height = sc.nextInt();
-		System.out.println("select length");
+		System.out.println("choisir largeur");
 		int length = sc.nextInt();
-		int[] rtn = {height, length};
+		int[] rtn = { height, length };
 		return rtn;
 	}
 
+	public static int selectHeight() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("select height");
+		// String input
+		return sc.nextInt();
+	}
+
+	public static int selectLength() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("select length");
+		// String input
+		return sc.nextInt();
+	}
+
+	public static Complex selectLeftTopComplex() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose the top left point of the rectangle");
+		System.out.println("Choose the real part : ");
+		int x = sc.nextInt();
+		System.out.println("Choose the imaginary part : ");
+		int y = sc.nextInt();
+		return new Complex(x, y);
+	}
+
+	public static double selectStep() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose the step : ");
+		return sc.nextDouble();
+	}
+
+	public static int[] selectJulia() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("choisir plus grand indice de x dans z");
+		int tabsize = sc.nextInt() + 1;
+		int[] poly = new int[tabsize];
+		for (int i = 0; i < tabsize; i++) {
+			System.out.println("choisir le facteur de x^" + i);
+			poly[i] = sc.nextInt();
+		}
+		return poly;
+	}
+
+	public static Complex selectJulia2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("choisir la partie entiere de c dans la formule f(x)=z+c, z est un polynome de x");
+		double real = sc.nextDouble();
+		System.out.println("choisir la partie imaginaire");
+		double imaginary = sc.nextDouble();
+		Complex c = new Complex(real, imaginary);
+		return c;
+
+	}
+
 	public static void main(String[] args) {
-		Complex x = new Complex(-0.7269, 0.1889);
-		Calcul m = new Calcul();
-		m.comp=x;
+		
+		
+		/*
+		 * // Complex x = new Complex(-0,7269, 0,1889); Calcul m = new Calcul(); m.comp
+		 * = selectJulia2(); m.polynome = selectJulia();
+		 * System.out.print("fonction: f(x)="); for (int i = m.polynome.length - 1; i >=
+		 * 0; i--) System.out.print(m.polynome[i] + "" + "x" + "^" + i + "+ ");
+		 * System.out.print(m.comp); m.fill(); ImageGenerator g = new ImageGenerator();
+		 * g.imageDrawer(m.calculate()); View v = new View(g.image);
+		 * Calcul m = new Calcul(selectHeight(), selectLength(), selectStep(), selectLeftTopComplex());
+		m.comp = x;
 		m.fill();
-		ImageGenerator g = new ImageGenerator();
 		g.imageDrawer(m.calculate());
 		View v = new View(g.image);
+		 */
 	}
 }
