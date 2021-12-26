86b9ce9cd283694fbe966a7317be57ea22d97891
Alass <alassane.paris@gmail.com>
2021-12-26 20:41:17 +0100

    conflits

--- ProjetFrac/src/Main.java
+++ ProjetFrac/src/Main.java
@@ -37,70 +37,10 @@ public class Main {
 		int height = sc.nextInt();
 		System.out.println("choisir largeur");
 		int length = sc.nextInt();
-<<<<<<< ProjetFrac/src/Main.java
 		int[] rtn = { height, length };
-=======
-		int[] rtn = {height, length};
-		sc. close();
->>>>>>> ProjetFrac/src/Main.java
 		return rtn;
 	}
-	public static int selectHeight() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("select height");
-		// String input
-		int ret = sc.nextInt();
-		return ret;
-	}
-	public static int selectLength() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("select length");
-		// String input
-		int ret = sc.nextInt();
-		
-		return ret;
-	}
-	public static Complex selectLeftTopComplex() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Choose the top left point of the rectangle");
-		System.out.println("Choose the real part : ");
-		int x = sc.nextInt();
-		System.out.println("Choose the imaginary part : ");
-		int y = sc.nextInt();
-		return new Complex(x,y);	
-	}	
-	public static double selectStep() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Choose the step : ");
-		double ret = sc.nextDouble();
-		
-		return ret;
-	}
-	public static int[] selectJulia() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("choisir plus grand indice de x dans z");
-		int tabsize = sc.nextInt()+1;
-		int[] poly = new int[tabsize];
-		for (int i = 0; i < tabsize; i++) {
-			System.out.println("choisir le facteur de x^" + i);
-			poly[i] = sc.nextInt();
-		}
-		
-		return poly;
-	}
 
-	public static Complex selectJulia2() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("choisir la partie reelle de c dans la formule f(x)=z+c, z est un polynome de x");
-		double real = sc.nextDouble();
-		System.out.println("choisir la partie imaginaire");
-		double imaginary = sc.nextDouble();
-		Complex c = new Complex(real, imaginary);
-		return c;
-
-	}
-
-<<<<<<< ProjetFrac/src/Main.java
 	public static int selectHeight() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("select height");
@@ -171,31 +111,5 @@ public class Main {
 		 * m.comp = x; m.fill(); g.imageDrawer(m.calculate()); View v = new
 		 * View(g.image);
 		 */
-=======
-	//public static void main(String[] args) {
-		//Complex x = new Complex(-0.7269, 0.1889);
-		//	-0,7269
-		// 0,1889
-		/*Calcul m = new Calcul();
-		m.comp = selectJulia2();
-		m.polynom = selectJulia();
-		System.out.print("fonction: f(x)=");
-		for (int i = m.polynom.length-1; i >= 0; i--)
-			System.out.print(m.polynom[i]+""+"x"+"^"+i+"+ ");
-		System.out.print(m.comp);
-		m.fill();
-		ImageGenerator g = new ImageGenerator();
-		g.imageDrawer(m.calculate());
-		View v = new View(g.image);*/
-		public static void main(String[] args) {
-			Complex x = new Complex(-0.7269, 0.1889);
-			Calcul m = new Calcul(selectHeight(), selectLength(), selectStep(), selectLeftTopComplex());
-			m.comp = selectJulia2();
-			m.polynom=selectJulia();
-			m.fill();
-			ImageGenerator g = new ImageGenerator();
-			g.imageDrawer(m.calculate());
-			View v = new View(g.image);
->>>>>>> ProjetFrac/src/Main.java
 	}
 }
